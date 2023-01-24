a8a430ee928c9c64112b0135f6e5133cda0006b2
Ayşe Akışık <ayseakisik16@gmail.com>
2023-01-25 00:10:55 +0300

    mysql bağlantısı

--- routes/user.js
+++ routes/user.js
@@ -29,16 +29,42 @@ const data = {
     ]
 }
 
-router.use("/blogs/blogid", function(req, res) {
+const db = require("../data/db");
+
+router.use("/blogs/blogid", function (req, res) {
     res.render("users/blog-details")
 })
 
-router.use("/blogs", function(req, res) {
-    res.render("users/blogs", data)
+router.use("/blogs", function (req, res) {
+    db.execute("select * from blog where onay=1")
+        .then(result => {
+            db.execute("select * from category")
+                .then(cresult => {
+                    res.render("users/blogs", {
+                        title: "Tüm Kurslar",
+                        blogs: result[0],
+                        categories: cresult[0]
+                    });
+                })
+                .catch(err => console.log(err))
+        })
+        .catch(err => console.log(err));
 })
 
-router.use("/", function(req, res) {
-    res.render("users/index", data)
+router.use("/", function (req, res) {
+    db.execute("select * from blog where onay=1")
+        .then(result => {
+            db.execute("select * from category")
+                .then(cresult => {
+                    res.render("users/index", {
+                        title: "Popüler Kurslar",
+                        blogs: result[0],
+                        categories: cresult[0]
+                    });
+                })
+                .catch(err => console.log(err))
+        })
+        .catch(err => console.log(err));
 })
 
 module.exports = router;
