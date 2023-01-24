b24df0cd0918df899db1b7efb952bca68b36bef6
Ayşe Akışık <ayseakisik16@gmail.com>
2023-01-24 14:50:39 +0300

    partial view a dinamik veri gönderme

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,44 @@
 const express = require("express");
-const path = require("path");
-
 const router = express.Router();
+
+const data = {
+    title: "Popüler Kurslar",
+    categories: ["Web Geliştirme", "Programlama", "Mobil Uygulamalar", "Veri Analizi", "Ofis Uygulamaları"],
+    blogs: [
+        {
+            blogid: 1,
+            baslik: "baslik1",
+            aciklama: "aciklama1",
+            resim: "1.jpeg",
+            anasayfa: true
+        },
+        {
+            blogid: 2,
+            baslik: "baslik2",
+            aciklama: "aciklama2",
+            resim: "2.jpeg",
+            anasayfa: true
+        },
+        {
+            blogid: 3,
+            baslik: "baslik3",
+            aciklama: "aciklama3",
+            resim: "3.jpeg",
+            anasayfa: false
+        }
+    ]
+}
 
 router.use("/blogs/blogid", function(req, res) {
     res.render("users/blog-details")
 })
 
 router.use("/blogs", function(req, res) {
-    res.render("users/blogs")
+    res.render("users/blogs", data)
 })
 
 router.use("/", function(req, res) {
-    res.render("users/index")
+    res.render("users/index", data)
 })
 
 module.exports = router;
